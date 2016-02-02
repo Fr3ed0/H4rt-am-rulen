b586949e4d6b4f0d97f15c56ebff5caf91557c47
Fr3ed0 <zocker_alf@web.de>
2016-02-02 18:29:29 +0100

    Create motor_regelung.ino

---
+++ motor_regelung.ino
@@ -0,0 +1,63 @@
+//Copyright Frederic "Fre3d0" Wolf
+//2015-2016
+
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+#include "utility/Adafruit_PWMServoDriver.h"
+#include <math.h>
+
+
+Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
+Adafruit_DCMotor *rightMotor = AFMS.getMotor(1);
+Adafruit_DCMotor *leftMotor = AFMS.getMotor(2);
+
+//#define DEBUG
+byte a = digitalRead(19);
+byte b = digitalRead(20);
+int now;
+int zeit = 100;
+int k;
+int g;
+int h;
+int tempoa;
+int tempob;
+int tempoaMax;
+int tempobMax;
+int tempo;
+
+void setup() {
+  Serial.begin(9600);           // set up Serial library at 9600 bps
+  Serial.println("Adafruit Motorshield v2 - DC Motor test!");
+
+  AFMS.begin();  // create with the default frequency 1.6KHz
+  
+  leftMotor->run(FORWARD);
+  rightMotor->run(FORWARD);
+
+  
+  #ifdef DEBUG
+  rightMotor->setSpeed(255);
+  #endif
+}
+
+void loop() {
+ if((millis() - now) > zeit){
+    now = millis();
+    h++;
+ }
+  g = h % 2;
+  if(g == 0){
+    if(a > 0){
+      tempob = 0;
+      tempoa ++;
+      }
+  
+  }
+  if(g == 1){
+    tempoaMax = tempoa;
+    tempoa = 0;
+    tempob ++;
+  }
+  
+    tempo = tempoaMax + tempobMax;
+}
